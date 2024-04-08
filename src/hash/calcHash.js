b8b43a40dd8397f747e2f824839d25c1a88500d0
wickedside <w1ck3ds1d3@gmail.com>
2024-04-08 22:31:53 +0300

    Done hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+const fs = require('fs').promises;
+const crypto = require('crypto');
+const path = require('path');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'fileToCalculateHashFor.txt');
+    try {
+        const data = await fs.readFile(filePath);
+        const hash = crypto.createHash('sha256');
+        hash.update(data);
+        const hexHash = hash.digest('hex');
+        console.log(hexHash);
+    } catch (error) {
+        console.error('Error reading or hashing file:', error.message);
+    }
 };
 
 await calculateHash();
