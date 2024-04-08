938bfceb30ac4118047ee2e0eaa49cb1659784fc
wickedside <w1ck3ds1d3@gmail.com>
2024-04-08 20:29:36 +0300

    Done create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+const fs = require('fs').promises;
+const path = require('path');
+
 const create = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', 'fresh.txt');
+    try {
+        await fs.access(filePath);
+        throw new Error('FS operation failed');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            await fs.writeFile(filePath, 'I am fresh and young');
+        } else {
+            throw error;
+        }
+    }
 };
 
 await create();
