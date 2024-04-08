20c3490386405fbe0b6796a75b317bb9d1e35b6c
wickedside <w1ck3ds1d3@gmail.com>
2024-04-08 21:14:36 +0300

    Revert "Done list.js"
    This reverts commit cfb794c45b9c093763acde9da1a8b9618c74f51b.

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,14 +1,5 @@
-const fs = require('fs').promises;
-const path = require('path');
-
 const list = async () => {
-    const dirPath = path.join(__dirname, 'files');
-    try {
-        const files = await fs.readdir(dirPath);
-        console.log(files);
-    } catch (error) {
-        throw new Error('FS operation failed');
-    }
+    // Write your code here 
 };
 
 await list();
