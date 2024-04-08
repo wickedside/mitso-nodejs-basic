7735f719b22bf86b7957715ca8021ff66f15a250
wickedside <w1ck3ds1d3@gmail.com>
2024-04-08 21:36:43 +0300

    Done copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,39 @@
+const fs = require('fs').promises;
+const path = require('path');
+
 const copy = async () => {
-    // Write your code here 
+    const srcDir = path.join(__dirname, 'files');
+    const destDir = path.join(__dirname, 'files_copy');
+
+    async function copyRecursive(src, dest) {
+        await fs.mkdir(dest, { recursive: true });
+        let entries = await fs.readdir(src, { withFileTypes: true });
+        for (let entry of entries) {
+            let srcPath = path.join(src, entry.name);
+            let destPath = path.join(dest, entry.name);
+            if (entry.isDirectory()) {
+                await copyRecursive(srcPath, destPath);
+            } else {
+                await fs.copyFile(srcPath, destPath);
+            }
+        }
+    }
+
+    try {
+        await fs.access(srcDir);
+        try {
+            await fs.access(destDir);
+            throw new Error('FS operation failed');
+        } catch (error) {
+            if (error.code === 'ENOENT') {
+                await copyRecursive(srcDir, destDir);
+            } else {
+                throw error;
+            }
+        }
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await copy();
