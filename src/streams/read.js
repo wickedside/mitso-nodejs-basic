0538b4c5d615d28be85e7e11de4f0ef7121ac861
wickedside <w1ck3ds1d3@gmail.com>
2024-04-08 21:33:28 +0300

    Done read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
 const read = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'fileToRead.txt');
+    const readStream = fs.createReadStream(filePath, 'utf-8');
+    readStream.pipe(process.stdout);
 };
 
 await read();
