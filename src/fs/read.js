1b59739909f2501dbbca7d09140de4c960c753d8
wickedside <w1ck3ds1d3@gmail.com>
2024-04-08 20:39:00 +0300

    Done read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+const fs = require('fs').promises;
+const path = require('path');
+
 const read = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+    try {
+        const content = await fs.readFile(filePath, 'utf8');
+        console.log(content);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
