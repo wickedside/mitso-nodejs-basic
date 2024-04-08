d2f242ba790fa6785d1456e3b8bea9173567eb28
wickedside <w1ck3ds1d3@gmail.com>
2024-04-08 22:45:47 +0300

    Done child processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,25 @@
+const { spawn } = require('child_process');
+const path = require('path');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const scriptPath = path.join(__dirname, 'script.js');
+
+    // создаем дочерний процесс
+    const child = spawn('node', [scriptPath, ...args], { stdio: ['inherit', 'inherit', 'inherit', 'ipc'] });
+
+    // передаем данные из stdin основного процесса в stdin дочернего процесса
+    process.stdin.pipe(child.stdin);
+
+    // перенаправляем stdout дочернего процесса в stdout основного процесса
+    child.stdout.on('data', (data) => {
+        process.stdout.write(data);
+    });
+
+    // обработка события завершения дочернего процесса
+    child.on('close', (code) => {
+        console.log(`Child process exited with code ${code}`);
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+// для тестирования добавьте аргументы в вызов функции
+spawnChildProcess(['arg1', 'arg2']);
