6ecdb71196e71140387ea0388b4126b55c070b5b
wickedside <w1ck3ds1d3@gmail.com>
2024-04-08 20:36:05 +0300

    Done rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,14 @@
+const fs = require('fs').promises;
+const path = require('path');
+
 const rename = async () => {
-    // Write your code here 
+    const srcPath = path.join(__dirname, 'files', 'wrongFilename.txt');
+    const destPath = path.join(__dirname, 'files', 'properFilename.md');
+    try {
+        await fs.rename(srcPath, destPath);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await rename();
