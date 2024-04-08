4e7f2beb12d16508ab2c54b9a3037b0a6a44b69d
wickedside <w1ck3ds1d3@gmail.com>
2024-04-08 22:20:46 +0300

    Done zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,15 @@
+const fs = require('fs');
+const zlib = require('zlib');
+const path = require('path');
+
 const compress = async () => {
-    // Write your code here 
+    const fileToCompress = path.join(__dirname, 'fileToCompress.txt');
+    const compressedFile = path.join(__dirname, 'archive.gz');
+    const readStream = fs.createReadStream(fileToCompress);
+    const writeStream = fs.createWriteStream(compressedFile);
+    const gzip = zlib.createGzip();
+
+    readStream.pipe(gzip).pipe(writeStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+const fs = require('fs');
+const zlib = require('zlib');
+const path = require('path');
+
 const decompress = async () => {
-    // Write your code here 
+    const compressedFile = path.join(__dirname, 'archive.gz');
+    const decompressedFile = path.join(__dirname, 'fileToCompress.txt');
+    const readStream = fs.createReadStream(compressedFile);
+    const writeStream = fs.createWriteStream(decompressedFile);
+    const gunzip = zlib.createGunzip();
+
+    readStream.pipe(gunzip).pipe(writeStream);
 };
 
 await decompress();
